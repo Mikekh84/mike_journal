8da12e92a71756044d71987ce7e029ef6a481ddf
Mike <mikekh@uw.edu>
2016-08-23 10:07:26 -0700

    Step1 with htmls and stuff.

--- learning_journal/learning_journal/routes.py
+++ learning_journal/learning_journal/routes.py
@@ -1,6 +1,7 @@
 def includeme(config):
     """Include Me config thingy."""
     config.add_route('home', '/')
-    config.add_route('detail', '/detail/')
-    config.add_route('edit', '/edit/' )
+    config.add_route('create', '/journal/new-entry/')
+    config.add_route('update', r'/journal/{id:\d+}/edit-entry/')
+    config.add_route('detail', r'/journal/{id:\d+}/')
     config.add_static_view('static', 'static', cache_max_age=3600)
--- learning_journal/learning_journal/views.py
+++ learning_journal/learning_journal/views.py
@@ -4,12 +4,33 @@ import os
 HERE = os.path.dirname(__file__)
 
 
-def my_view(request):
-    """View first view."""
-    imported_text = open(os.path.join(HERE, 'mock.html')).read()
-    return Response(imported_text)
+def list_view(request):
+    """Home page/index of learning journal."""
+    index = open(os.path.join(HERE, 'index.html')).read()
+    return Response(index)
+
+
+def create_view(request):
+    """View for create."""
+    created = open(os.path.join(HERE, 'create.html')).read()
+    return Response(created)
+
+
+def update_view(request):
+    """Edit view."""
+    edit = open(os.path.join(HERE, 'editone.html')).read()
+    return Response(edit)
+
+
+def detail_view(request):
+    """Journal Entry view."""
+    journal = open(os.path.join(HERE, 'journalentryone.html')).read()
+    return Response(journal)
 
 
 def includeme(config):
     """Include me for views."""
-    config.add_view(my_view, route_name='home')
+    config.add_view(list_view, route_name='home')
+    config.add_view(create_view, route_name='create')
+    config.add_view(update_view, route_name='update')
+    config.add_view(detail_view, route_name='detail')
