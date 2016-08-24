9bf47c1e0ff066ccb001bb872b45f4b52be84fdd
Mike <mikekh@uw.edu>
2016-08-24 16:47:18 -0700

    moved learning_journal up one level.

--- learning_journal/tests.py
+++ learning_journal/tests.py
@@ -3,10 +3,11 @@ from pyramid import testing
 
 
 def test_detail_view_has_tittle():
+    """Test detail view has title thingy."""
     from .views import detail_view
     request = testing.DummyRequest()
     info = detail_view(request)
-    assert "title" in info.keys
+    assert "Journal Entry One" in info
 
 
 @pytest.fixture()
@@ -17,6 +18,7 @@ def testapp():
     from webtest import TestApp
     return TestApp(app)
 
+
 def test_layout_root(testapp):
-    response - testapp.get('/', status=200)
+    response = testapp.get('/', status=200)
     assert b'soemthig' in response.body
