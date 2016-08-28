f6f1ed9f4c016c4c7798f77305f470ee9b2dc35d
Mike <mikekh@uw.edu>
2016-08-28 15:52:30 -0700

    it works but no tests. Will do test later. IM SORRYgit add .!

--- learning_journal/views.py
+++ learning_journal/views.py
@@ -1,16 +1,21 @@
 from pyramid.view import view_config
+from .journal import JOURNAL_ENTRIES as JOURNAL_ENTRIES
+from pyramid.exceptions import HTTPNotFound
 
 
 @view_config(route_name="home", renderer="templates/index.jinja2")
 def list_view(request):
     """View home and list of journals."""
-    return {}
+    return {"entries": JOURNAL_ENTRIES}
 
 
 @view_config(route_name="edit", renderer="templates/edit.jinja2")
 def edit_view(request):
     """View the edit view."""
-    return {}
+    for entry in JOURNAL_ENTRIES:
+        if entry['id'] == request.matchdict["id"]:
+            return {"entry": entry}
+    return HTTPNotFound
 
 
 @view_config(route_name="create", renderer="templates/create.jinja2")
@@ -22,4 +27,7 @@ def create_view(request):
 @view_config(route_name="detail", renderer="templates/journal.jinja2")
 def detail_view(request):
     """View the detail view."""
-    return {}
+    for entry in JOURNAL_ENTRIES:
+        if entry['id'] == request.matchdict["id"]:
+            return {"entry": entry}
+    return HTTPNotFound
