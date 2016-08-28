dcd24e6bf02c2462a7183c80b0d45c157c3b6057
Mike <mikekh@uw.edu>
2016-08-28 14:36:11 -0700

    Started step views and templates.

--- learning_journal/__init__.py
+++ learning_journal/__init__.py
@@ -5,9 +5,8 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
-    config.include('pyramid_chameleon')
+    config.include('pyramid_jinja2')
     config.include('.routes')
-    config.include('.views')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.scan()
     return config.make_wsgi_app()
--- learning_journal/views.py
+++ learning_journal/views.py
@@ -1,36 +1,25 @@
-from pyramid.response import Response
-import os
-
-HERE = os.path.dirname(__file__)
+from pyramid.view import view_config
 
 
+@view_config(route_name="home", renderer="templates/index.jinja2")
 def list_view(request):
-    """View the lists."""
-    imported_list = open(os.path.join(HERE,'templates/index.html')).read()
-    return Response(imported_list)
+    """View home and list of journals."""
+    return {}
 
 
+@view_config(route_name="edit", renderer="templates/edit.jinja2")
 def edit_view(request):
     """View the edit view."""
-    edit = open(os.path.join(HERE, 'templates/editone.html')).read()
-    return Response(edit)
+    return {}
 
 
+@view_config(route_name="create", renderer="templates/create.jinja2")
 def create_view(request):
     """View the create view."""
-    create = open(os.path.join(HERE, 'templates/create.html')).read()
-    return Response(create)
+    return {}
 
 
+@view_config(route_name="detail", renderer="templates/journal.jinja2")
 def detail_view(request):
     """View the detail view."""
-    detail = open(os.path.join(HERE, 'templates/journalentryone.html')).read()
-    return Response(detail)
-
-
-def includeme(config):
-    """Include Me!."""
-    config.add_view(list_view, route_name="home")
-    config.add_view(edit_view, route_name='edit')
-    config.add_view(create_view, route_name='create')
-    config.add_view(detail_view, route_name='detail')
+    return {}
