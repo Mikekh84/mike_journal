11ecd82fd58cf96dacd24a7ad4931b7acb80cc28
Mike <mikekh@uw.edu>
2016-09-17 12:31:32 -0700

    Fixed some issues with accessing the database.

--- learning_journal/scripts/initializedb.py
+++ learning_journal/scripts/initializedb.py
@@ -36,7 +36,7 @@ def main(argv=sys.argv):
     engine = get_engine(settings)
     Base.metadata.create_all(engine)
 
-    # session_factory = get_session_factory(engine)
+    session_factory = get_session_factory(engine)
 
     # with transaction.manager:
     #     dbsession = get_tm_session(session_factory, transaction.manager)
