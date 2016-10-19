65f6aa284f4104cb3896555dc1e1c638d68e3f5a
mikekh84 <mikekh@uw.edu>
2016-10-18 19:44:47 -0700

    Added a test for basisc 200 for login.

--- learning_journal/tests/test_security.py
+++ learning_journal/tests/test_security.py
@@ -20,3 +20,9 @@ def test_private_view(testapp):
     """Test access to a private view."""
     response = testapp.get('/new-entry/', status='4*')
     assert response.status_code == 403
+
+
+def test_login_view(testapp):
+    """Test access to login view is 200 public."""
+    response = testapp.get('/login/')
+    assert response.status_code == 200
